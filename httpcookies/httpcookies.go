c0bcd64ca76a266db8d01fa3be6d3a2a9eabc672
Sasha Varlamov <sasha@sashavarlamov.com>
2017-12-21 19:26:19 +0800

    Add domain to cookies

--- httpcookies/httpcookies.go
+++ httpcookies/httpcookies.go
@@ -7,31 +7,34 @@ import (
 )
 
 // SetJavascriptAccessibleCookie should NOT UNDER ANY CIRCUMSTANCES be used for sensitive credentials! It sets an HTTP cookie that *is* accessible through the client Javascript. This should only be done for insecure variables that are useful to the client
-func SetJavascriptAccessibleCookie(w http.ResponseWriter, expires time.Time, cookieName string, cookieValue interface{}) {
+func SetJavascriptAccessibleCookie(w http.ResponseWriter, expires time.Time, domain string, cookieName string, cookieValue interface{}) {
 	http.SetCookie(w, &http.Cookie{
 		Name:    cookieName,
 		Value:   fmt.Sprint(cookieValue),
+		Domain: domain,
 		Path:    "/",
 		Expires: expires,
 	})
 }
 
 // SetHTTPOnlyCookie sets a cookie that is only visible to the browser, as opposed to the client JS itself. This makes these cookies safe for access tokens and other credentials
-func SetHTTPOnlyCookie(w http.ResponseWriter, expires time.Time, cookieName string, cookieValue interface{}) {
+func SetHTTPOnlyCookie(w http.ResponseWriter, expires time.Time, domain string, cookieName string, cookieValue interface{}) {
 	http.SetCookie(w, &http.Cookie{
 		Name:     cookieName,
 		Value:    fmt.Sprint(cookieValue),
 		Path:     "/",
 		HttpOnly: true,
 		Expires:  expires,
+		Domain: domain,
 	})
 }
 
 // DeleteCookie removes a cookie from the clients browser, useful for things like getting rid of a client's session cookie
-func DeleteCookie(w http.ResponseWriter, cookieName string) {
+func DeleteCookie(w http.ResponseWriter, domain string, cookieName string) {
 	http.SetCookie(w, &http.Cookie{
 		Name:   cookieName,
 		MaxAge: -1,
 		Path:   "/",
+		Domain: domain,
 	})
 }
