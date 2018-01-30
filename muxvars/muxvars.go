bf085ab860aa4e4e6a066f4fe4f8c58f8ce89ed0
Sasha Varlamov <sasha@sashavarlamov.com>
2018-01-31 01:35:09 +0700

    Update mux vars utils

--- muxvars/muxvars.go
+++ muxvars/muxvars.go
@@ -7,6 +7,32 @@ import (
 	"strconv"
 )
 
+// GetStringVar tries to get the var with the key
+func GetStringVar(r *http.Request, key string) string {
+	vars := mux.Vars(r)
+	if _, ok := vars[key]; !ok {
+		return ""
+	}
+	return vars[key]
+}
+
+// GetUnsignedIntegerVar tries to get the var with the key and parse it into a base 10 uint
+func GetUnsignedIntegerVar(r *http.Request, key string) uint {
+	vars := mux.Vars(r)
+	if _, ok := vars[key]; !ok {
+		return 0
+	}
+	if len(vars[key]) < 1 {
+		return 0
+	}
+	value, err := strconv.ParseUint(vars[key], 10, 64)
+	if err != nil {
+		return 0
+	}
+	return uint(value)
+}
+
+// GetIntegerVar tries to get the var with the key and parse it into a base 10 int64
 func GetIntegerVar(r *http.Request, key string) int64 {
 	vars := mux.Vars(r)
 	if _, ok := vars[key]; !ok {
@@ -22,6 +48,7 @@ func GetIntegerVar(r *http.Request, key string) int64 {
 	return value
 }
 
+// GetURLBase64EncodedStringVar tries to get the var with the key and then parse it into a string using the b64 URL decoder
 func GetURLBase64EncodedStringVar(r *http.Request, key string) string {
 	vars := mux.Vars(r)
 	if _, ok := vars[key]; !ok {
